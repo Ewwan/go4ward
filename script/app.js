695f0fd8a87ee9778630c86cbe2c9529b1e3cbec
Ewwan <ewaiwan29@gmail.com>
2019-05-31 18:37:58 +0200

    urlToFetch

--- script/app.js
+++ script/app.js
@@ -1,11 +1,11 @@
 //Foursquare API info
-const clientID = '';
-const clientSecret = '';
-const url = '';
+const clientID = '2H51FMANDAQ23IQDIZADKXFGVMIOO5YXT0VHGVC2RMELXVAX';
+const clientSecret = 'JYPY4IKK2QBXMSLB1V4TFBSVYW4A2QILTYBOJ42QK3QKF1CW';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 //APIXU Info
-const apiKey = '';
-const forecastUrl = '';
+const apiKey = 'fcadfff8557741d4a5d122327193105';
+const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=';
 
 //Page Elements
 const $input = $('#city');
@@ -18,8 +18,27 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednseday', 'Thursday', 'Wedne
 
 // Add AJAX functions here:
 const getVenues = () => {
+    const city = $input.val();
+    const currentDate = () => {
+        const today = new Date();
+        const yyyy = today.getFullYear();
+        const mm = today.getMonth()+1;
+        const dd = today.getDate();
+        const doubleDigit = (digit) => {
+            if(digit < 10){
+                return '0' + digit;
+            } else {
+                return digit;
+            }
+        }
+        const currentDateFormat = '' + yyyy + doubleDigit(mm) + doubleDigit(dd);
+        return currentDateFormat;
+    };
 
-}
+    const urlToFetch = '' + url + city + '&limit=10' + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + currentDate();
+    console.log(urlToFetch);
+    return urlToFetch;
+};
 
 const getForecast = () => {
 
